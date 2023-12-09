948d26acc16a942dfe0d60f53f1fef322b0742fd
jopak19 <jpaulogomessiqueira@gmail.com>
2023-12-09 20:43:36 -0300

    Adiciona configuração de segurança e dependência web mvc

---
+++ src/main/java/com/imd/escola/configurations/SecurityConfig.java
@@ -0,0 +1,34 @@
+package com.imd.escola.configurations;
+
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
+        return  http
+                .csrf(csrf -> csrf.disable())
+                .authorizeHttpRequests(autorized -> autorized
+                        .requestMatchers(HttpMethod.GET, "/**").permitAll()
+                        .requestMatchers(HttpMethod.POST, "/**").permitAll()
+                        .requestMatchers(HttpMethod.DELETE, "/**").permitAll()
+                        .requestMatchers(HttpMethod.PUT, "/**").permitAll()
+                        .anyRequest().authenticated()
+
+                ).build();
+    }
+
+
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
+}
